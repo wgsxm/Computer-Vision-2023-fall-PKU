db7c1c888d857918701994a0d3276b036f01cc05
wgsxm <2901199020@qq.com>
2023-10-05 04:13:59 +0800

    finished 3.1

--- Problem Set 1/gaussian_pyramid.py
+++ Problem Set 1/gaussian_pyramid.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy import signal
+
 def cross_correlation_2d(X,K):
     height,width = K.shape
     Y = np.zeros((X.shape[0]-height+1,X.shape[1]-width+1))
@@ -17,10 +17,4 @@ def convolve_2d(X,K):
             Y[i][j]=(X[i:i+height, j:j+height]*K).sum()
     return np.flip(np.flip(Y, axis=0), axis=1)
 
-matrix = np.array([[1, 2, 3],
-                   [4, 5, 6],
-                   [7, 8, 9],
-                   [10,11,12]])
-K = np.array([[0,0],[1,0]])
-print(signal.convolve2d(matrix,K,'valid'))
-print(convolve_2d(matrix,K))
+
