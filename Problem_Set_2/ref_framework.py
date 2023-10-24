55e9643734c80db6a1ca4301fd789606398e4458
wgsxm <2901199020@qq.com>
2023-10-25 02:13:40 +0800

    try to finish task 1

--- Problem_Set_2/ref_framework.py
+++ Problem_Set_2/ref_framework.py
@@ -1,18 +1,46 @@
 # This is a raw framework for image stitching using Harris corner detection.
 # For libraries you can use modules in numpy, scipy, cv2, os, etc.
-
-
+import scipy, cv2
+import numpy as np
+import matplotlib.pyplot as plt
+file_path = './Problem_Set_2/Problem2Images/1_2.jpg'
+def rgb_to_gray(img):
+    if len(img.shape) == 3:
+        gray_image = np.mean(img, axis=2).astype(np.float64)
+    else:
+        gray_image = img
+    return gray_image
+def gaussian_blur_kernel_2d(kernel_size=3,sigma=1):
+    kernel=np.zeros((kernel_size,kernel_size))
+    r = kernel_size//2
+    for x in range(-r,r+1):
+        for y in range(-r,r+1):
+            kernel[x+r,y+r] = 1/(2*np.pi*sigma**2)*np.exp(-1/(2*sigma**2)*(x**2+y**2))
+    kernel = kernel / np.sum(kernel)
+    return kernel
+def convolve_2d(X,K):
+    height,width = K.shape
+    X = np.flip(np.flip(X, axis=0), axis=1)
+    Y = np.zeros((X.shape[0]-height+1,X.shape[1]-width+1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i][j]=(X[i:i+height, j:j+height]*K).sum()
+    return np.flip(np.flip(Y, axis=0), axis=1)
 def gradient_x(img):
     # convert img to grayscale
     # should we use int type to calclate gradient?
     # should we conduct some pre-processing to remove noise? which kernel should we pply?
     # which kernel should we choose to calculate gradient_x?
     # TODO
-    return grad_x
+    gray_image = rgb_to_gray(img)
+    filtered_image = scipy.ndimage.gaussian_filter(gray_image, sigma = 1, mode = 'reflect')
+    return scipy.ndimage.sobel(filtered_image, axis = 0, mode = 'reflect')
 
 def gradient_y(img):
     # TODO
-    return grad_y
+    gray_image = rgb_to_gray(img)
+    filtered_image = scipy.ndimage.gaussian_filter(gray_image, sigma = 1, mode = 'reflect')
+    return scipy.ndimage.sobel(filtered_image, axis = 1, mode = 'reflect')
 
 def harris_response(img, alpha, win_size):
     # In this function you are going to claculate harris response R.
@@ -21,15 +49,43 @@ def harris_response(img, alpha, win_size):
     # remember to smooth the gradients. 
     # Avoid using too much "for" loops to speed up.
     # TODO
+    win_size = win_size // 2
+    grad_x = gradient_x(img)
+    grad_y = gradient_y(img)
+    sigma = win_size / 5
+    
+    kernel = gaussian_blur_kernel_2d(win_size, sigma)
+    A = grad_x * grad_x
+    B = grad_x * grad_y
+    C = grad_y * grad_y
+    R = np.zeros_like(A)
+    for i in range(win_size, img.shape[0] - win_size):
+        for j in range(win_size, img.shape[1] - win_size):
+            windows_A = np.sum(A[i - win_size:i + win_size + 1, j - win_size:j + win_size + 1] * kernel)
+            windows_B = np.sum(B[i - win_size:i + win_size + 1, j - win_size:j + win_size + 1] * kernel)
+            windows_C = np.sum(C[i - win_size:i + win_size + 1, j - win_size:j + win_size + 1] * kernel)
+            det_R = windows_A * windows_C - windows_B * windows_B
+            tr_R = windows_A + windows_C
+            windows_R = det_R - sigma * tr_R**2
+            R[i, j] = windows_R
     return R
 
-def corner_selection(R, thresh, min_dist):
+def corner_selection(R, th, min_dist):
     # non-maximal suppression for R to get R_selection and transform selected corners to list of tuples
     # hint: 
     #   use ndimage.maximum_filter()  to achieve non-maximum suppression
     #   set those which aren’t **local maximum** to zero.
     # TODO
-    return pix
+    R = scipy.ndimage.maximum_filter(R, size = min_dist)
+    threshold = th * np.max(R)
+    condition_matrix = (R > threshold).astype(np.uint8)
+    pix = []
+    for i in range(1, R.shape[0] - 1):
+        for j in range(1, R.shape[1] - 1):
+            if condition_matrix[i, j]:
+                #pix += [[i, j], [i - 1, j - 1], [i - 1, j], [i - 1, j + 1], [i, j - 1], [i, j + 1], [i + 1, j - 1], [i + 1, j], [i + 1, j + 1]]
+                pix.append([i,j])
+    return list(pix)
 
 def histogram_of_gradients(img, pix):
     # no template for coding, please implement by yourself.
@@ -87,4 +143,12 @@ if __name__ == '__main__':
     # make image list
     # call generate panorama and it should work well
     # save the generated image following the requirements
-    
+    image = cv2.imread(file_path)
+    R = harris_response(image,0.05,3)
+    target = corner_selection(R,0.001,3)
+    output = np.zeros_like(R)
+    image = rgb_to_gray(image)
+    for i in target:
+        image[i[0],i[1]]=255
+    plt.imshow(image)
+    plt.show()
