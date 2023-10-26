1abf30db1a7590574e8abae58a461891fc1073d4
wgsxm <2901199020@qq.com>
2023-10-26 20:11:25 +0800

    modified original file

--- Problem_Set_2/ref_framework.py
+++ Problem_Set_2/ref_framework.py
@@ -4,6 +4,7 @@ import scipy, cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from random import sample
+from matplotlib.patches import ConnectionPatch
 def rgb_to_gray(img):
     if len(img.shape) == 3:
         gray_image = np.mean(img, axis=2).astype(np.float64)
@@ -48,10 +49,10 @@ def harris_response(img, alpha=0.05, win_size=3):
     # Avoid using too much "for" loops to speed up.
     # TODO
     kernel = gaussian_blur_kernel_2d(win_size, win_size/7)
-    A = grad_x * grad_x
     win_size = win_size // 2
     grad_x = gradient_x(img)
     grad_y = gradient_y(img)
+    A = grad_x * grad_x
     B = grad_x * grad_y
     C = grad_y * grad_y
     R = np.zeros_like(A)
@@ -97,8 +98,8 @@ def histogram_of_gradients(img, pix):
     #   7. Then rotate it’s neighbor to fit principle orientation and calculate the histogram again. 
     # TODO
     WIDTH,HEIGHT = img.shape
-    block_size = 4  # 块大小
-    num_bins = 8  # 方向直方图的箱数
+    block_size = 4  
+    num_bins = 8 
     grad_x = gradient_x(img)
     grad_y = gradient_y(img)
     grad_magnitude = np.sqrt(grad_x**2,grad_y**2)
@@ -106,16 +107,15 @@ def histogram_of_gradients(img, pix):
     features = []
     for cornel in pix:
         cornel_x, cornel_y = cornel
-        # 选角点周围的区域
-        # 先算出主方向
         temp_feature = [0 for i in range(num_bins)]
         for i in range(cornel_x - block_size**2//2 + 1, cornel_x + block_size**2//2 + 1):
             for j in range(cornel_y - block_size**2//2 + 1, cornel_y + block_size**2//2 + 1):
-                    temp_feature[int(grad_direction[np.clip(i,0,WIDTH-1),np.clip(j,0,HEIGHT-1)]*num_bins/360)]+=grad_magnitude[np.clip(i,0,WIDTH-1),int(np.clip(j,0,HEIGHT-1))]
+                    temp_angle = int(grad_direction[np.clip(i,0,WIDTH-1),np.clip(j,0,HEIGHT-1)]*num_bins/360)
+                    if temp_angle == num_bins:
+                        temp_angle = num_bins - 1
+                    temp_feature[temp_angle]+=grad_magnitude[np.clip(i,0,WIDTH-1),int(np.clip(j,0,HEIGHT-1))]
         orientation = np.argmax(temp_feature)*360/num_bins
         feature = []
-        #print(change)
-        # 从右上角往下遍历, 旋转
         for index_x in range(-block_size//2,block_size//2):
             for index_y in range(-block_size//2,block_size//2):
                 temp_feature = [0 for i in range(num_bins)]
@@ -124,24 +124,22 @@ def histogram_of_gradients(img, pix):
                         temp_angle = grad_direction[np.clip(i,0,WIDTH-1),np.clip(j,0,HEIGHT-1)] - orientation
                         if temp_angle<0:
                              temp_angle+=360
-                        temp_feature[int(temp_angle*num_bins/360)]+=grad_magnitude[np.clip(i,0,WIDTH-1),np.clip(j,0,HEIGHT-1)]
+                        temp_angle = int(temp_angle*num_bins/360)
+                        if temp_angle == num_bins:
+                            temp_angle = num_bins - 1
+                        temp_feature[temp_angle]+=grad_magnitude[np.clip(i,0,WIDTH-1),np.clip(j,0,HEIGHT-1)]
                 feature+=temp_feature
-                #print(temp_feature)
         # 归一化
         feature = feature / np.sum(feature)
         features.append(feature)
     return features
-
-
-        
-    
 
 def feature_matching(img_1, img_2):
     # align two images using \verb|harris_response|, \verb|corner_selection|,
     # \verb|histogram_of_gradients|
     # hint: calculate distance by scipy.spacial.distance.cdist (using HoG features, euclidean will work well)
     # TODO
-    THRESHOLD =0.15
+    THRESHOLD =0.14
     corners_1 = corner_selection(harris_response(img_1,0.05,3),0.01,3)
     corners_2 = corner_selection(harris_response(img_2,0.05,3),0.01,3)
     features_1 = histogram_of_gradients(img_1, corners_1)
@@ -181,42 +179,28 @@ def compute_homography(pixels_1, pixels_2):
     return homo_matrix
 
 def align_pair(pixels_1, pixels_2):
-    
     # utilize \verb|homo_coordinates| for homogeneous pixels
     # and \verb|compute_homography| to calulate homo_matrix
     # implement RANSAC to compute the optimal alignment.
     # you can refer to implementations online.
-    num_iterations = 200  # Number of RANSAC iterations
+    num_iterations = 200 # Number of RANSAC iterations
     inlier_threshold = 5.0  # Threshold for considering a point as an inlier
     num_picked = 4
     best_inliers = []  # Best set of inliers
     best_est_homo = None  # Best estimated homography matrix
-
     for _ in range(num_iterations):
-        # Randomly sample 4 point correspondences
         sample_indices = sample(range(len(pixels_1)), num_picked)
         sampled_pixels_1 = [pixels_1[i] for i in sample_indices]
         sampled_pixels_2 = [pixels_2[i] for i in sample_indices]
-
-        # Calculate homography matrix using the sampled points
         est_homo = compute_homography(sampled_pixels_1, sampled_pixels_2)
-
-        # Transform all pixels from image 1 using the estimated homography
-        homo_coordinates_1 = np.array(pixels_1 + [(1,) for _ in range(len(pixels_1))])
+        homo_coordinates_1 = np.array([pixels_1[i] + tuple([1]) for i in range(len(pixels_1))])
         transformed_pixels = np.dot(est_homo, homo_coordinates_1.T).T
-        transformed_pixels = transformed_pixels[:, :2] / transformed_pixels[:, 2, None]
-
-        # Calculate the Euclidean distance between the transformed pixels and image 2 pixels
+        transformed_pixels = transformed_pixels[:, :2] / transformed_pixels[:, 2, None]+1e-6
         distances = np.linalg.norm(transformed_pixels - pixels_2, axis=1)
-
-        # Count the number of inliers based on the threshold
         inliers = np.where(distances < inlier_threshold)[0]
-
-        # Update the best set of inliers and homography matrix if this is the best so far
         if len(inliers) > len(best_inliers):
             best_inliers = inliers
             best_est_homo = est_homo
-
     return best_est_homo
 
 def stitch_blend(img_1, img_2, est_homo):
@@ -226,64 +210,59 @@ def stitch_blend(img_1, img_2, est_homo):
     # Together with four corner pixels of the other image, we can get the size of new image plane.
     # Then, remap both image to new image plane and blend two images using Alpha Blending.
     # TODO
-    W, H= img_1.shape[:2]
-    corners = np.array([[0, 0, 1], [W, 0, 1], [W, H, 1], [0, H, 1]])
-    projected_corners = np.dot(est_homo, corners.T).T
-    projected_corners /= projected_corners[:, 2, np.newaxis]
-    min_x = np.min(projected_corners[:,1])
-    max_x = np.max(projected_corners[:,1])
-    min_y = np.min(projected_corners[:,0])
-    max_y = np.max(projected_corners[:,0])
-    width,height = img_2.shape
-    alpha = 0.45
-    if min_x < 0:
-        height -= min_x
-    if min_y < 0:
-        width +=min_y   
-    print(img_1.shape)
-    print(img_2.shape)
-    print(min_x,min_y)
-    width = max(width,max_y)
-    height = max(height,max_x)
-    print(width,height)
-    est_img = np.zeros((int(width)+1,int(height)+1))
-    for i in range(W):
-        for j in range(H):
-            x,y = i,j
-            py,px=0,0
-            if min_x>0:
-                y = int(y+min_x)
+    # 先确定画布的大小，先算img1的四个角，把他弄到正的区域，然后算两幅图的起始位置
+    WIDTH_1,HEIGHT_1 = img_1.shape[:-1]
+    WIDTH_2,HEIGHT_2 = img_2.shape[:-1]
+    ALPHA = 0.5
+    homo_inverse = np.linalg.inv(est_homo)
+    corners = [(0,0,1),(WIDTH_1-1,0,1),(0,HEIGHT_1-1,1),(WIDTH_1-1,HEIGHT_1-1,1)]
+    startx_1,starty_1,endx_1,endy_1=float('inf'),float('inf'),-float('inf'),-float('inf')
+    for corner in corners:
+        modified_corner = est_homo@corner
+        modified_corner = modified_corner/modified_corner[2]
+        startx_1 = int(min(startx_1,modified_corner[0]))
+        endx_1 = int(max(endx_1,modified_corner[0]))
+        starty_1 = int(min(starty_1,modified_corner[1]))
+        endy_1 = int(max(endy_1,modified_corner[1]))
+    startx_2,starty_2,endx_2,endy_2=0,0,WIDTH_2,HEIGHT_2
+    if startx_1<0: #平移图像
+        startx_2-=startx_1
+        endx_2-=startx_1
+    else:
+        startx_1=0
+    if starty_1<0: #平移图像 
+        starty_2-=starty_1
+        endy_2-=starty_1
+    else:
+        starty_1=0
+    EST_WIDTH = max(endx_1,endx_2)+1
+    EST_HEIGHT = max(endy_1,endy_2)+1
+    est_img = np.zeros((EST_WIDTH,EST_HEIGHT,3))
+    # 写一个函数来确定点是否在重合的区域中，逆矩阵
+    def is_inside(point):
+        modified_point = homo_inverse@point
+        modified_point = modified_point / modified_point[2]
+        return (0<modified_point[0]<WIDTH_1 and 0<modified_point[1]<HEIGHT_1),modified_point
+    # 先画img2
+    for i in range(WIDTH_2):
+        for j in range(HEIGHT_2):
+            x,y=startx_2+i,starty_2+j
+            if is_inside([x+startx_1,y+starty_1,1])[0]:
+                est_img[x,y] = img_2[i,j]*ALPHA
             else:
-                px = -min_x
-            if min_y>0:
-                x = int(x+min_y)
-            else:
-                py = -min_y
-            if y>px and py <= x <= img_2.shape[0]+py:
-                 est_img[x,y]=img_1[i,j]*alpha
-            else:
-                est_img[x,y]=\
-                img_1[i,j]
-    for i in range(img_2.shape[0]-1):
-        for j in range(img_2.shape[1]-1):
-            x,y=i,j
-            px,py=0,0
-            if min_x<0:
-                y-=min_x
-            else:
-                px =min_x
-            if min_y<0:
-                x-=min_y
-            else:
-                py =min_y
-            x = int(x)
-            y = int(y)
-            if y<px+img_1.shape[1] and py <= x <= img_2.shape[0]+py:
-                 est_img[x,y]+=img_2[i,j]*(1-alpha)
-            else:
-                est_img[x,y]+=img_2[i,j]
-    return est_img
-
+                est_img[x,y] = img_2[i,j]
+    # 然后画img1,这里可能需要插值，先不插
+    for i in range(EST_WIDTH):
+        for j in range(EST_HEIGHT):
+            flag, target = is_inside([i+startx_1,j+starty_1,1])     
+            if flag:
+                x,y=int(target[0]),int(target[1])
+                # 如果在img2中
+                if startx_2<i<endx_2 and starty_2<j<endy_2:
+                    est_img[i,j]+=img_1[x,y]*(1-ALPHA)
+                else:
+                    est_img[i,j] = img_1[x,y]
+    return np.array(est_img,dtype=np.uint8)
 
 def generate_panorama(ordered_img_seq):
     # finally we can use \verb|feature_matching| \verb|align_pair| and \verb|stitch_blend| to generate 
@@ -291,8 +270,22 @@ def generate_panorama(ordered_img_seq):
     # TODO
     return est_panorama
 
+def match_visualizer(image,image_2,pix_1,pix_2):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.imshow(image, cmap='gray')
+    ax1.set_title('Image 1')
+    # 在第二个子图上显示第二幅图像
+    ax2.imshow(image_2, cmap='gray')
+    ax2.set_title('Image 2')
+    for i in range(len(pix_1)):
+        ax1.plot(pix_1[i][1],pix_1[i][0],'ro',markersize=1,color='red')
+        ax2.plot(pix_2[i][1],pix_2[i][0],'ro',markersize=1,color='red')
+        con = ConnectionPatch(xyA=(pix_1[i][1],pix_1[i][0]), xyB=(pix_2[i][1],pix_2[i][0]), linewidth=0.1, axesA= ax1,axesB=ax2,coordsA='data',
+                        coordsB='data',color="red")
+        ax2.add_artist(con)
+    plt.show()
+
 if __name__ == '__main__':
     # make image list
     # call generate panorama and it should work well
     # save the generated image following the requirements
-    pass
