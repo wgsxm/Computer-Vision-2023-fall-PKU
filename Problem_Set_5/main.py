4038dcfcb21d4dac2da4651f034ce40f7356bc44
wgsxm <2901199020@qq.com>
2023-12-07 11:59:29 +0800

    try to finish lab5

--- Problem_Set_5/main.py
+++ Problem_Set_5/main.py
@@ -1,17 +1,19 @@
 import torch
 import torch.nn as nn
 import argparse
-
-
+import torchvision
+import torchvision.transforms as transforms
+import os
+PATH = './model.pt'
 class LinearClassifier(nn.Module):
     # define a linear classifier
     def __init__(self, in_channels, out_channels):
         super().__init__()
         # inchannels: dimenshion of input data. For example, a RGB image [3x32x32] is converted to vector [3 * 32 * 32], so dimenshion=3072
         # out_channels: number of categories. For CIFAR-10, it's 10
-
+        self.linear = nn.Linear(in_channels, out_channels)
     def forward(self, x: torch.Tensor):
-        return 
+        return self.linear(x)
 
 
 class FCNN(nn.Module):
@@ -26,9 +28,15 @@ class FCNN(nn.Module):
         # activation function
         # full connected layer
         # ......
+        self.fc1 = nn.Linear(in_channels, hidden_channels)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_channels, out_channels)
 
     def forward(self, x: torch.Tensor): 
-        return 
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
     
 
 def svmloss(scores: torch.Tensor, label: torch.Tensor):
@@ -40,6 +48,10 @@ def svmloss(scores: torch.Tensor, label: torch.Tensor):
     return:
         svm loss
     '''
+    correct_scores = scores[torch.arange(scores.size(0)), label].view(-1, 1)
+    margins = torch.clamp(scores - correct_scores + 1, min=0)
+    margins[torch.arange(scores.size(0)), label] = 0  # Ignore the correct class
+    loss = margins.sum() / scores.size(0)
 
     return loss
 
@@ -52,9 +64,10 @@ def crossentropyloss(logits: torch.Tensor, label: torch.Tensor):
     return: 
         cross entropy loss
     '''
-    # softmax function
-    # cross entropy
 
+    exp_logits = torch.exp(logits)
+    softmax_probs = exp_logits / exp_logits.sum(dim=1, keepdim=True)
+    loss = -torch.log(softmax_probs[torch.arange(logits.size(0)), label]).mean()
     return loss
 
 
@@ -68,28 +81,65 @@ def train(model, loss_function, optimizer, scheduler, args):
         scheduler: step or cosine
         args: configuration
     '''
+    if os.path.exists(PATH):
+        checkpoint = torch.load(PATH)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
     # create dataset
-
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    batch_size = 4
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     # create dataloader
-
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
     # for-loop 
+    epoch_cnt = 2
+    running_loss = 0.0
+    for epoch in range(epoch_cnt):
         # train
-            # get the inputs; data is a list of [inputs, labels]
-
+        # get the inputs; data is a list of [inputs, labels]
+        for i, data in enumerate(trainloader):
+            inputs, labels = data
+            inputs = nn.Flatten()(inputs)
             # zero the parameter gradients
-
+            optimizer.zero_grad()
             # forward
-
+            outputs = model(input) 
             # loss backward
-
+            loss = loss_function(outputs, labels)
+            loss.backward()
             # optimize
-
+            optimizer.step()
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
         # adjust learning rate
-
+        scheduler.step()
         # test
+        correct = 0
+        total = 0
+        with torch.no_grad():
             # forward
+            for data in testloader:
+                images, labels = data
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
 
     # save checkpoint (Tutorial: https://pytorch.org/tutorials/recipes/recipes/saving_and_loading_a_general_checkpoint.html)
+    torch.save(
+        {
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'scheduler_state_dict': scheduler.state_dict()
+        }, PATH)
 
 def test(model, loss_function, args):
     '''
@@ -98,11 +148,28 @@ def test(model, loss_function, args):
         loss_function: SVM loss of Cross-entropy loss
     '''
     # load checkpoint (Tutorial: https://pytorch.org/tutorials/recipes/recipes/saving_and_loading_a_general_checkpoint.html)
+    checkpoint = torch.load(PATH)
+    model.load_state_dict(checkpoint['model_state_dict'])
     # create testing dataset
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    batch_size = 4
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     # create dataloader
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
     # test
+    correct = 0
+    total = 0
+    with torch.no_grad():
         # forward
-
+        for data in testloader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='The configs')
@@ -116,29 +183,29 @@ if __name__ == '__main__':
 
     # create model
     if args.model == 'linear':
-        model = 
+        model = LinearClassifier(3 * 32 * 32, 10)
     elif args.model == 'fcnn':
-        model = 
+        model = FCNN(3 * 32 * 32, 1024, 10)
     else: 
         raise AssertionError
 
     # create optimizer
     if args.optimizer == 'adamw':
         # create Adamw optimizer
-        optimizer = 
+        optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.01)
     elif args.optimizer == 'sgd':
         # create SGD optimizer
-        optimizer = 
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.01)
     else:
         raise AssertionError
     
     # create scheduler
     if args.scheduler == 'step':
         # create torch.optim.lr_scheduler.StepLR scheduler
-        scheduler = 
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.1)
     elif args.scheduler == 'cosine':
         # create torch.optim.lr_scheduler.CosineAnnealingLR scheduler
-        scheduler = 
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, step_size=100, gamma=0.1)
     else:
         raise AssertionError
 
